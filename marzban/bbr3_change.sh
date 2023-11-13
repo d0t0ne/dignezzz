74218c99549f837d1b793d88e49ca53776396a94
Alexey <50312583+DigneZzZ@users.noreply.github.com>
2023-11-13 23:38:36 +0300

    Update bbr3_change.sh

--- marzban/bbr3_change.sh
+++ marzban/bbr3_change.sh
@@ -4,13 +4,20 @@
 clear
 
 # Вывод заголовка
+echo '  
+                           
+BBBB  Y   Y     DDD  III  GGG  N   N EEEE ZZZZZ ZZZZZ ZZZZZ 
+B   B  Y Y      D  D  I  G     NN  N E       Z     Z     Z  
+BBBB    Y       D  D  I  G  GG N N N EEE    Z     Z     Z   
+B   B   Y       D  D  I  G   G N  NN E     Z     Z     Z    
+BBBB    Y       DDD  III  GGG  N   N EEEE ZZZZZ ZZZZZ ZZZZZ 
+                                                            
+
+'
+sleep 2s
 echo -e "\e[1m\e[33mДанный скрипт устанавливает ядро с поддержкой BBR3\n=================================================================\e[0m"
-
-# Пауза на 4 секунды
-sleep 4
-
+sleep 1
 echo -e "\e[33mОпределяем доступную версию ядра\e[0m"
-
 # Функция для выполнения команды и обработки вывода
 run_command() {
     result=$(eval $1)
@@ -24,7 +31,7 @@ run_command() {
 # Получение информации о CPU
 cpu_info=$(run_command "cat /proc/cpuinfo")
 
-# Определение уровня поддержки
+# Определение уровня 
 level=0
 if [[ $cpu_info =~ lm|cmov|cx8|fpu|fxsr|mmx|syscall|sse2 ]]; then
     level=1
@@ -46,7 +53,7 @@ if [ $level -gt 0 ]; then
     echo -e "\e[33mДобавляем репозиторий\e[0m"
     run_command "echo 'deb [signed-by=/usr/share/keyrings/xanmod-archive-keyring.gpg] http://deb.xanmod.org releases main' | sudo tee /etc/apt/sources.list.d/xanmod-release.list"
 
-    # Выполнить соответствующее действие в зависимости от уровня
+    # Выполняем соответствующее действие в зависимости от уровня
     echo -e "\e[33mУстанавливаем ядро, не закрывайте окно\e[0m"
     run_command "sudo apt update && sudo apt install -y linux-xanmod-x64v$level"
 
