7fe5c2d6d34861c9231df92c28fa90875f135a98
Alexey <50312583+DigneZzZ@users.noreply.github.com>
2023-11-13 23:40:39 +0300

    Update sysctl_opt.sh

--- server/sysctl_opt.sh
+++ server/sysctl_opt.sh
@@ -8,22 +8,15 @@ bold=$(tput bold)
 green=$(tput setaf 2)
 yellow=$(tput setaf 3)
 reset=$(tput sgr0)
-
-# Очистка экрана
 clear
-
-# Вывод заголовка
 echo "${yellow}Данный скрипт оптимизирует сеть, путем выставления параметров SYSCTL${reset}"
-
 echo "${yellow}Делаем бекап файла${reset}"
 cp ${sysctl_path} /etc/sysctl.conf.backup
 echo "Файл сохранен по пути /etc/sysctl.conf.backup"
 sleep 1s
 echo "${yellow}Скачиваем новый файл sysctl.conf${reset}"
 wget "https://raw.githubusercontent.com/DigneZzZ/dignezzz.github.io/main/server/sysctl.conf" -q -O  ${sysctl_path}
-
 echo "${yellow}Перезапускаем сеть${reset}"
 sysctl -p
-
 echo "${yellow}Готово!${reset}"
 
